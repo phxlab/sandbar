1df8fe5664eff8daf9fa38c5e1b5fd50a7a61e4c
Nico Plyley <nico@luxwaymedia.com>
2023-02-18 23:24:07 -0600

    :lipstick: Added desktop styles

--- src/lib/blocks/home/Header.svelte
+++ src/lib/blocks/home/Header.svelte
@@ -3,12 +3,14 @@
 </script>
 
 <Header background={imageHandler('home/kitchen-header.webp', true)}>
-	<div class="py-20 text-center">
-		<h1 class="pb-4 text-grey-100">
+	<div class="py-20 text-center lg:w-1/3 lg:py-40 lg:text-left">
+		<h1 class="text-grey-100">
 			<span class="block font-body text-xl font-normal">Sandbar</span>
 			Full Service Facilities Contractor
 		</h1>
-		<p class="pb-4 text-grey-100">Expert solutions for all your commercial facilities needs</p>
+		<p class="py-4 text-grey-100 lg:py-7">
+			Expert solutions for all your commercial facilities needs
+		</p>
 		<Button href="/contact">Request A Quote</Button>
 	</div>
 </Header>
