29df502e6efa336a9669fa512eef6aaecdb0d14a
Nico Plyley <nico@luxwaymedia.com>
2023-02-18 18:55:21 -0600

    :sparkles: Created container component

---
+++ src/lib/components/container/index.svelte
@@ -0,0 +1,9 @@
+<!--suppress JSUnusedGlobalSymbols -->
+<script>
+	let className = '';
+	export { className as class };
+</script>
+
+<div class="container mx-auto px-2.5 {className}">
+	<slot />
+</div>
