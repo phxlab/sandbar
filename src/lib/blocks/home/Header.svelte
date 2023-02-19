9abf92d3fbca061e7987b63cd647954a028d5ee6
Nico Plyley <nico@luxwaymedia.com>
2023-02-18 23:14:45 -0600

    :sparkles: Created header block

---
+++ src/lib/blocks/home/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Button, Header, imageHandler } from '$lib';
+</script>
+
+<Header background={imageHandler('home/kitchen-header.webp', true)}>
+	<div class="py-20 text-center">
+		<h1 class="pb-4 text-grey-100">
+			<span class="block font-body text-xl font-normal">Sandbar</span>
+			Full Service Facilities Contractor
+		</h1>
+		<p class="pb-4 text-grey-100">Expert solutions for all your commercial facilities needs</p>
+		<Button href="/contact">Request A Quote</Button>
+	</div>
+</Header>
