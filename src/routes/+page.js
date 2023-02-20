f5d1ceba8bd16a681ad487bead39f72b4a18e28f
Nico Plyley <nico@luxwaymedia.com>
2023-02-20 14:31:47 -0600

    :sparkles: Created content server page

---
+++ src/routes/+page.js
@@ -0,0 +1,54 @@
+export let load;
+load = () => {
+	return {
+		seo: {
+			title: 'Full-Service Facilities Contractor | Sandbar Mechanical',
+			description:
+				'Your trusted full-service commercial contractor. Contact us for quality solutions at competitive prices.',
+		},
+		header: {
+			upperTitle: 'Sandbar Mechanical',
+			title: 'Full-Service Facilities Contractor',
+			description: 'Expert solutions for all your commercial facilities needs',
+			button: {
+				title: 'Request A Quote',
+				href: '/contact',
+			},
+		},
+		services: {
+			title: 'Our Services',
+			items: [
+				{
+					icon: 'hvac',
+					title: 'Commercial HVAC',
+					description:
+						'Authoritatively disseminate revolutionary partnerships without interdependent.',
+				},
+				{
+					icon: 'refrigeration',
+					title: 'Commercial Refrigeration',
+					description:
+						'Authoritatively disseminate revolutionary partnerships without interdependent.',
+				},
+				{
+					icon: 'plumbing',
+					title: 'Commercial Plumbing',
+					description:
+						'Authoritatively disseminate revolutionary partnerships without interdependent.',
+				},
+				{
+					icon: 'kitchen',
+					title: 'Kitchen Equipment',
+					description:
+						'Authoritatively disseminate revolutionary partnerships without interdependent.',
+				},
+				{
+					icon: 'electrical',
+					title: 'Electrical',
+					description:
+						'Authoritatively disseminate revolutionary partnerships without interdependent.',
+				},
+			],
+		},
+	};
+};
