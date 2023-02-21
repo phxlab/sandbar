6f7a2c68f6f5338bde7a95e6b381f7f6510c5576
Nico Plyley <nico@luxwaymedia.com>
2023-02-21 12:47:26 -0600

    :lipstick: Updated image styles

--- src/lib/blocks/home/Faqs.svelte
+++ src/lib/blocks/home/Faqs.svelte
@@ -5,12 +5,12 @@
 	const { title, items, image } = data;
 </script>
 
-<Container class="py-20">
-	<h2 class="mb-10 text-center">{title}</h2>
+<Container class="my-20">
+	<h2 class="mb-20 text-center">{title}</h2>
 	<div class="flex justify-between">
 		<Faqs {items} />
 		<img
-			class="ml-20 hidden flex-shrink-0 rounded-2xl xl:block xl:w-[650px] 2xl:w-[720px]"
+			class="ml-20 hidden h-[1023px] flex-shrink-0 rounded-2xl object-cover xl:block xl:w-[650px] 2xl:w-[720px]"
 			src={imageHandler(image.path, image.dev)}
 			alt={image.alt}
 		/>
