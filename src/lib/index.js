40197d6f275b5111aa60ef7d6715e7f547ab6a7c
Nico Plyley <nico@luxwaymedia.com>
2023-02-21 12:42:35 -0600

    :sparkles: Exported components

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,6 +4,7 @@ export { default as Container } from './components/container/index.svelte';
 export { default as Header } from './components/header/index.svelte';
 export { default as Button } from './components/button/index.svelte';
 export { default as ServiceCards } from './components/service-cards/index.svelte';
+export { default as Faqs } from './components/faqs/index.svelte';
 
 // Utils
 export { default as imageHandler } from './utils/imageHandler';
