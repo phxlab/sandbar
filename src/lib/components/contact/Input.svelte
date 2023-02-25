607bd53f57b37ae2e65b44cac7af493495a323e2
Nico Plyley <nico@luxwaymedia.com>
2023-02-25 16:35:39 -0500

    :sparkles: Created initial contact form

---
+++ src/lib/components/contact/Input.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let type = 'text';
+	export let label = 'Enter A Label';
+	export let name = '';
+</script>
+
+<div class="mb-7">
+	<label for={name}>
+		{label}
+	</label>
+	<input
+		class="mt-2 block w-full border-b border-grey-300 px-2 py-2 active:border-primary-500"
+		id={name}
+		{name}
+		{type}
+		placeholder={label}
+	/>
+</div>
