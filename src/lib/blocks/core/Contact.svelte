7c569697398e40923eddf5fa9ffeae605a50bcb4
Nico Plyley <nico@luxwaymedia.com>
2023-02-25 16:35:54 -0500

    :sparkles: Created contact block

---
+++ src/lib/blocks/core/Contact.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { Contact, Container, imageHandler } from '$lib';
+
+	export let data;
+	const { background } = data;
+</script>
+
+<div
+	class="py-10"
+	style={`
+	background: linear-gradient(90deg, rgba(20, 3, 40, 0.7) 0%, rgba(28, 5, 45, 0) 100%),
+							url('${imageHandler(background.path)}');
+	background-size: cover;
+	background-position: center center;
+	`}
+>
+	<Container class="flex h-full items-center justify-center">
+		<Contact />
+	</Container>
+</div>
