cafa367190f6f4a83d7b48f7895b2fb3890b3dcb
Nico Plyley <nico@phoenixtechlab.com>
2023-02-25 18:26:22 -0500

    :recycle: Refactored to seperate file

---
+++ src/lib/components/contact/ContactForm.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Input from './form/Input.svelte';
+	import Select from './form/Select.svelte';
+	import { services, times } from './form/options.js';
+	import { Button } from '$lib';
+</script>
+
+<div class="w-full p-4 lg:px-10 lg:py-7">
+	<h3 class="mb-7">Contact Form</h3>
+	<div class="md:flex md:gap-5">
+		<Input name="firstName" label="First Name" />
+		<Input name="lastName" label="Last Name" />
+	</div>
+	<div class="md:flex md:gap-5">
+		<Input name="phone" label="Phone Number" type="tel" />
+		<Input name="email" label="Email" type="email" />
+	</div>
+	<div class="md:flex md:gap-5">
+		<Select name="time" label="Preferred Callback Time" placeholder="Select Time" options={times} />
+		<Select name="service" label="Service" placeholder="Select Service" options={services} />
+	</div>
+	<div>
+		<label for="message"> Message </label>
+		<textarea
+			name="message"
+			id="message"
+			class="min-h-24 mt-2 block h-24 w-full resize-y border-b border-grey-300 px-2 py-2 active:border-primary-500"
+			placeholder="Message"
+		/>
+	</div>
+	<div class="mt-7">
+		<Button>Submit</Button>
+	</div>
+</div>
---
+++ src/lib/components/contact/ContactInfo.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { contactStore, hoursStore, imageHandler } from '$lib';
+</script>
+
+<div
+	class="rounded-br-2xl rounded-bl-2xl bg-primary-500 p-4 md:py-10 lg:rounded-bl-none lg:rounded-tr-2xl lg:pr-14"
+>
+	<h4 class="mb-7 text-grey-100">Contact Info</h4>
+	{#each $contactStore as info}
+		<div class="mb-4 flex items-center gap-4">
+			<img src={imageHandler(info.icon)} alt="Call Us" />
+			<h4 class="text-grey-100">
+				<a href={`${info.href}${info.title}`}>{info.title}</a>
+			</h4>
+		</div>
+	{/each}
+
+	<hr class="border-t-primary-600" />
+
+	<!-- Hours -->
+	<div>
+		<h4 class="my-7 text-grey-100">Hours</h4>
+		{#each $hoursStore as info}
+			<div class="mb-4 flex items-center gap-4">
+				<h4 class="text-grey-100">{info.day}:</h4>
+				<p class="text-grey-100">{info.time}</p>
+			</div>
+		{/each}
+		<p class="text-grey-100">Emergency Services Available</p>
+	</div>
+</div>
