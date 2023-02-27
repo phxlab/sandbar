390c239d037696bf980c2fb72342ea21890510f4
Nico Plyley <nico@phoenixtechlab.com>
2023-02-27 02:29:11 -0500

    :sparkles: Created mobile nav

---
+++ src/lib/components/Navigation/MobileMenu.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { page } from '$app/stores';
+	import { navigationStore } from '$lib';
+
+	export let open;
+
+	const toggleMobileNav = () => {
+		open = false;
+	};
+</script>
+
+<div class="nav absolute z-30 w-full bg-primary-500">
+	<div class="flex flex-col">
+		{#each $navigationStore as item}
+			<a
+				on:click={toggleMobileNav}
+				class={`${
+					$page.url.pathname === item.href && 'bg-primary-600'
+				} p-7 text-lg text-grey-100 active:bg-primary-800`}
+				href={item.href}
+			>
+				{item.title}
+			</a>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.nav {
+		height: calc(100vh - 56px);
+		top: 56px;
+		left: 0;
+	}
+
+	:global(html) {
+		overflow: hidden;
+	}
+</style>
