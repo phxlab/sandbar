be5f104b73bd0048ac15c7855f6703dd19be7060
Nico Plyley <nico@phoenixtechlab.com>
2023-02-26 22:26:42 -0500

    :sparkles: Created new stores

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -2,3 +2,5 @@ import { writable } from 'svelte/store';
 
 export const contactStore = writable({});
 export const hoursStore = writable({});
+export const navigationStore = writable({});
+export const servicesStore = writable({});
