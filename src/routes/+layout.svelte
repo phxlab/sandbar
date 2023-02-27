438e1db3db35675af8698fb4b416191827a0fcb6
Nico Plyley <nico@phoenixtechlab.com>
2023-02-26 22:27:50 -0500

    :sparkles: Added footer and set data for new stores

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,22 @@
 <script>
 	import '../app.css';
-	import { contactStore, hoursStore, Navigation } from '$lib';
+	import {
+		contactStore,
+		Footer,
+		hoursStore,
+		Navigation,
+		navigationStore,
+		servicesStore,
+	} from '$lib';
 
-	export let data;
+	export let data = {};
 
 	contactStore.set(data.contact);
 	hoursStore.set(data.hours);
+	navigationStore.set(data.navigation.links);
+	servicesStore.set(data.services);
 </script>
 
 <Navigation />
 <slot />
+<Footer data={data.footer} />
