2cf7c898611cd293d24349ba42161b2be68198d1
Nico Plyley <nico@phoenixtechlab.com>
2023-02-26 22:25:58 -0500

    :sparkles: Created dynamic icon component

---
+++ src/lib/components/footer/Icon.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { onMount } from 'svelte';
+
+	export let data = {};
+	const { type, href } = data;
+	let Icon;
+
+	onMount(async () => {
+		Icon = (await import(`./icons/${type}.svelte`)).default;
+	});
+</script>
+
+<a {href} target="_blank" rel="noreferrer nofollow">
+	<svelte:component this={Icon} />
+</a>
